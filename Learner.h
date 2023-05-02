bb78e8ae9cb064a10954d876be323e943ab5596c
mduarte321 <126204218+mduarte321@users.noreply.github.com>
2023-05-02 13:36:12 -0500

    Update Learner.h

--- Learner.h
+++ Learner.h
@@ -1,11 +1,1 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <vector>
-using namespace std;
 
-#include "StockCollection.h"
-
-void TrainNN(StockCollection& sc);
-
-void RecentEvaluation(StockCollection& sc);
