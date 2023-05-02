3312a2ffccb5dc28bf7690a158018414bce3adcf
mduarte321 <126204218+mduarte321@users.noreply.github.com>
2023-05-02 08:48:16 -0500

    Learner.h

---
+++ Learner.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+#include "StockCollection.h"
+
+void TrainNN(StockCollection& sc);
+
+void RecentEvaluation(StockCollection& sc);
